c685371d721456b32c6d5b6c9926c013b3ae254d
NicoleLatifi <Nicole.Latifi@gmail.com>
2022-12-13 09:18:32 -0700

    refactor: convert CardList

--- src/components/CardList/CardList.component.jsx
+++ src/components/CardList/CardList.component.jsx
@@ -1,20 +1,14 @@
-import { Component } from "react";
-
 import "./CardList.styles.css";
 import Card from "../Card/Card.component";
 
-class CardList extends Component {
-  render() {
-    const { monsters } = this.props;
-
-    return (
-      <div className="card-list">
-        {monsters.map((monster) => {
-          return <Card key={monster.id} monster={monster} />;
-        })}
-      </div>
-    );
-  }
-}
+const CardList = ({ monsters }) => {
+  return (
+    <div className="card-list">
+      {monsters.map((monster) => {
+        return <Card key={monster.id} monster={monster} />;
+      })}
+    </div>
+  );
+};
 
 export default CardList;
